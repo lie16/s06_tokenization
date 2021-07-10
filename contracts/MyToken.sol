abf614cd38f408a669cb141ba544aa5a575ac7f5
lie16 <yonattan_hope@yahoo.com>
2021-07-10 09:22:48 +0800

    add MyToken

---
+++ contracts/MyToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("StarDucks Capu-Token", "SCT") public {
+        _mint(msg.sender, initialSupply);
+    _setupDecimals(0);
+    }
+}
