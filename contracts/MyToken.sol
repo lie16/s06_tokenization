63603145e3723bee6ef1336a20d11087529d7519
lie16 <yonattan_hope@yahoo.com>
2021-07-10 10:07:22 +0800

    removing _setupDecimals

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -5,8 +5,8 @@ pragma solidity >=0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("StarDucks Capu-Token", "SCT") public {
+    constructor(uint256 initialSupply) ERC20("StarDucks Capu-Token", "SCT") {
         _mint(msg.sender, initialSupply);
-    _setupDecimals(0);
+        //_setupDecimals(0);
     }
 }
