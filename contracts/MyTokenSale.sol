2e98a1094866d9294c44bf1ab1e973ead211eead
lie16 <yonattan_hope@yahoo.com>
2021-07-10 11:56:36 +0800

    add MyTokenSale

---
+++ contracts/MyTokenSale.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.4;
+
+import "./Crowdsale.sol";
+
+contract MyTokenSale is Crowdsale {
+
+    KycContract kyc;
+    constructor(
+        uint256 rate,    // rate in TKNbits
+        address payable wallet,
+        IERC20 token
+    )
+        Crowdsale(rate, wallet, token)
+        public
+    {
+
+    }
+
+}
