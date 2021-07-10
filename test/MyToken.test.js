b9b38a1c0ee9fded66946a41741ad2142f2f2e4f
lie16 <yonattan_hope@yahoo.com>
2021-07-10 15:57:08 +0800

    modif tokentest to accomodate crowdsale test

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -11,12 +11,16 @@ chai.use(chaiAsPromised);
 
 const expect = chai.expect;
 
-contract("Token Test", async accounts => {
+contract("Token Test", function(accounts) {
     const [ initialHolder, recipient, anotherAccount ] = accounts;
 
+    beforeEach(async () => {
+      this.myToken = await Token.new(1000);
+    });
 
     it("All tokens should be in my account", async () => {
-    let instance = await Token.deployed();
+    //let instance = await Token.deployed();
+    let instance = this.myToken;
     let totalSupply = await instance.totalSupply();
     console.log("instance = " + instance);
     console.log("totalSupply = " + totalSupply);
@@ -31,7 +35,7 @@ contract("Token Test", async accounts => {
 
     it("I can send tokens from Account 1 to Account 2", async () => {
         const sendTokens = 1;
-        let instance = await Token.deployed();
+        let instance = this.myToken;
         let totalSupply = await instance.totalSupply();
         await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
         await expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;      
@@ -41,7 +45,7 @@ contract("Token Test", async accounts => {
   
   
       it("It's not possible to send more tokens than account 1 has", async () => {
-        let instance = await Token.deployed();
+        let instance = this.myToken;
         let balanceOfAccount = await instance.balanceOf(initialHolder);
         await expect(instance.transfer(recipient, new BN(balanceOfAccount+1))).to.eventually.be.rejected;
   
