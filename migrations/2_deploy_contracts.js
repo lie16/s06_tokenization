995eddd023cd8d422a7895c33094344a7ec10b70
lie16 <yonattan_hope@yahoo.com>
2021-07-12 09:24:52 +0800

    add KycContract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,8 +5,8 @@ require('dotenv').config({path: '../.env'});
 module.exports = async function(deployer) {
     let addr = await web3.eth.getAccounts();
     await deployer.deploy(MyToken, process.env.INITIAL_TOKENS);
-    //await deployer.deploy(MyToken, 1000000000);
-    await deployer.deploy(MyTokenSales, 1, addr[0], MyToken.address);
+    await deployer.deploy(KycContract);
+    await deployer.deploy(MyTokenSales, 1, addr[0], MyToken.address, KycContract.address);
     let instance = await MyToken.deployed();
     //await tokenInstance.transfer(MyTokenSales.address, 1000000000);
     await instance.transfer(MyTokenSales.address, process.env.INITIAL_TOKENS);
