f1d53c603166414fe88d08ce53e5c9996b2fceb4
lie16 <yonattan_hope@yahoo.com>
2021-07-12 09:26:37 +0800

    add KycContract

--- test/MyTokenSale.test.js
+++ test/MyTokenSale.test.js
@@ -1,5 +1,6 @@
 const TokenSale = artifacts.require("MyTokenSale");
 const Token = artifacts.require("MyToken");
+const KycContract = artifacts.require("KycContract");
 
 const chai = require("./chaisetup.js");
 const BN = web3.utils.BN;
@@ -30,6 +31,11 @@ it("should be possible to buy one token by simply sending ether to the smart con
     let tokenInstance = await Token.deployed();
     let tokenSaleInstance = await TokenSale.deployed();
     let balanceBeforeAccount = await tokenInstance.balanceOf.call(recipient);
+    await expect(tokenSaleInstance.sendTransaction({from: recipient, value: web3.utils.toWei("1", "wei")})).to.be.rejected;
+    await expect(balanceBeforeAccount).to.be.bignumber.equal(await tokenInstance.balanceOf.call(recipient));
+
+    let kycInstance = await KycContract.deployed();
+    await kycInstance.setKycCompleted(recipient);
 
     await expect(tokenSaleInstance.sendTransaction({from: recipient, value: web3.utils.toWei("1", "wei")})).to.be.fulfilled;
     return expect(balanceBeforeAccount + 1).to.be.bignumber.equal(await tokenInstance.balanceOf.call(recipient));
