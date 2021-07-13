9344253f3a11fb668466d9f4ce220dd744eb0b88
lie16 <yonattan_hope@yahoo.com>
2021-07-13 10:10:53 +0800

    done with error

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+require('dotenv').config({path: './.env'});
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const MetaMaskAccountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,6 +12,24 @@ module.exports = {
       port: 7545,
       network_id: "*",
       host: "127.0.0.1"
+    },
+    ganache_local: {
+      provider: function() {
+          return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:7545", MetaMaskAccountIndex )
+      },
+      network_id: 5777
+    },
+    goerli_infura: {
+      provider: function() {
+          return new HDWalletProvider(process.env.MNEMONIC, "https://goerli.infura.io/v3/8b08b0a3583c423094eba6c3fae0100d", MetaMaskAccountIndex )
+      },
+      network_id: 5
+    },
+    ropsten_infura: {
+      provider: function() {
+          return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/8b08b0a3583c423094eba6c3fae0100d", MetaMaskAccountIndex )
+      },
+      network_id: 3
     }
   },
   compilers: {    
